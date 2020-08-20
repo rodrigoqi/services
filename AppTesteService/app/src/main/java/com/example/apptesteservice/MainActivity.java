f743e85053823113c4f6824018e84638fa4b8183
Rodrigo Barreto <rodrigo.barreto@qi.edu.br>
2020-08-20 09:16:23 -0300

    projeto inicial só com a tela

---
+++ AppTesteService/app/src/main/java/com/example/apptesteservice/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.apptesteservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
